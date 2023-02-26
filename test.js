6e61e5a37721b2f455feb2cfb8bb0431b62abc2c
Manu Dwivedi <manu29.d@gmail.com>
2023-02-26 23:51:50 +0530

    Remove unnecessary variable declarations from test.js

--- test.js
+++ test.js
@@ -3,11 +3,11 @@ const db = require('./index').DB;
 const dbInstance = new db('namedb');
 
 const table = dbInstance.createTable('newTable');
-const row1 = dbInstance.insert({id: 1, name: 'foo1'}).into(table);
-const row2 = dbInstance.insert({id: 2, name: 'foo2'}).into(table);
-const row3 = dbInstance.insert({id: 3, name: 'foo3'}).into(table);
-const row4 = dbInstance.insert({id: 4, name: 'foo4'}).into(table);
-const query = dbInstance.select('*').from(table).where().orderBy('id', 'desc');
+dbInstance.insert({id: 1, name: 'foo1', gender: 'male'}).into(table);
+dbInstance.insert({id: 2, name: 'foo2', gender: 'female'}).into(table);
+dbInstance.insert({id: 3, name: 'foo3', gender: 'male'}).into(table);
+dbInstance.insert({id: 4, name: 'foo4', gender: 'female'}).into(table);
+const query = dbInstance.select('name', 'id').from(table).where({ gender: 'male' }).orderBy('id', 'desc');
 
 console.log(query.data);
 
